a27014c56a19ab4989c7c41c95a6f548d087be96
Tanzilur31 <156154754+Zilu45@users.noreply.github.com>
2024-01-23 13:30:01 +0700

    Update BangunRuang.java

--- BangunRuang.java
+++ BangunRuang.java
@@ -7,16 +7,5 @@ public class BangunRuang {
         System.out.println("Menghitung volume bangun ruang: ");
         return 0;
     }
-        float Bola(){
-        System.out.println("Menghitung volume bangun ruang: ");
-        return 0;
-    }
-            float Sisi(){
-        System.out.println("Me nghitung volume bangun ruang: ");
-        return 0;
-    }
-        float Ls(){
-        System.out.println("Menghitung volume bangun ruang: ");
-        return 0;
-    }
+
     }
